caebeb0c2017e02c7b986eaecf98f50f764aadde
AndriyYuzvenko <anbriy890@gmail.com>
2021-12-01 11:35:04 +0200

    додава домашку

--- homework8/additional/script.js
+++ homework8/additional/script.js
@@ -1,10 +1,12 @@
 // написати рекурсивну функцію, яка збирає всі назви класів з файлу rules.html в окремий масив. масив вивести в консоль
 
-let xxx = document.getElementsByClassName('rules')
-let arr = []
-let xxxFunction = (item) => {
 
-    return arr.push(item)
-}
-xxxFunction(xxx)
+let elem = document.querySelectorAll("div")
+let arr = []
+elem.forEach((item) => {
+    if (item.className.length > 1) {
+        arr.push(item.className)
+    }
+})
 console.log(arr)
+
